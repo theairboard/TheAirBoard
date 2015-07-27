cbaa20971dff9c9e7da7b2ee69115279528bbfff
The AirBoard <contact@theairboard.cc>
2015-07-27 11:48:39 +0200

    Update sketchBit

--- arduino/TheAirBoard/examples/sketchBit/sketchBit.ino
+++ arduino/TheAirBoard/examples/sketchBit/sketchBit.ino
@@ -37,7 +37,7 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   analogWrite(RED, analogRead(_5V) > 512 ? true:false); // set RED LED when POWER BIT is connected
-  analogWrite(BITOUT, map(constrain(analogRead(BITIN), 0, 820), 0, 820, 0, 255)); // redirect the input bit to the output bit
+  analogWrite(BITOUT, map(constrain(analogRead(BITIN), 0, 780), 0, 780, 0, 255)); // redirect the input bit to the output bit
   Serial.println(analogRead(BITIN));
   delay(500);                         // wait for 500 milliseconds
 }
